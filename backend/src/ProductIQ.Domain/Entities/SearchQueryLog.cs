b226a92024274c5d0cd962c9ba90f8b9af3517bf
Aygen5 <aygenyildirim27@gmail.com>
2026-08-21 12:03:06 +0300

    feat(domain): add SystemSetting and SearchQueryLog domain entities

---
+++ backend/src/ProductIQ.Domain/Entities/SearchQueryLog.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductIQ.Domain.Entities;
+
+using ProductIQ.Domain.Common;
+
+public class SearchQueryLog : BaseEntity
+{
+    public required string QueryText { get; set; }
+    public int ExecutionTimeMs { get; set; }
+    public int TotalResults { get; set; }
+    public decimal? AvgRelevanceScore { get; set; }
+}
---
+++ backend/src/ProductIQ.Domain/Entities/SystemSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductIQ.Domain.Entities;
+
+using ProductIQ.Domain.Common;
+
+public class SystemSetting : BaseEntity
+{
+    public required string Key { get; set; }
+    public required string Value { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+}
