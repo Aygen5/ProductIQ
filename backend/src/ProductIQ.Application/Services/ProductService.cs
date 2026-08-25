f7c5e0b68841a881f43302fea0fbfd8cf65c0902
Aygen5 <aygenyildirim27@gmail.com>
2026-08-25 16:17:24 +0300

    chore: ensure clean encoding and formatting across services

--- backend/src/ProductIQ.Application/Services/ProductService.cs
+++ backend/src/ProductIQ.Application/Services/ProductService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ProductIQ.Application.Common.Models;
 using ProductIQ.Application.DTOs;
 using ProductIQ.Application.Interfaces;
